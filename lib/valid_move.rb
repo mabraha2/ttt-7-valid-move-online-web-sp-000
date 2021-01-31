a56f05df696e4062f2191bbf4fabd30ae9055bff
Medhanei  Abraha <abrahama2019@gmail.com>
2021-01-31 02:21:02 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,6 @@
 def valid_move?(board, index)
-if board[index] == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+if board[index] == [" ", " ", " ", " ", " ", " ", " ", " ", " "] 
+  true 
 end
 
 #code your #valid_move? method here
